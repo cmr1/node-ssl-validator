7cb20d00acec8337ce313306e39a3401fab77608
Charlie McClung <charlie@cmr1.com>
2017-05-02 12:32:12 -0600

    Added tests

---
+++ test/ssl-validator.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+const SslValidator = require('../');
+
+describe('SslValidator', function () {
+    it('should exist', function () {
+        expect(SslValidator).to.exist;
+    });
+});
